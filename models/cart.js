2b83928e9ce6988b3f99e4120c59448d4dc0bb1f
Henil <henilp467@gmail.com>
2024-12-30 18:02:09 +1100

    add comments on what each function does in terms of cart functionality of stickers

--- models/cart.js
+++ models/cart.js
@@ -3,6 +3,8 @@ module.exports = function Cart(oldCart) {
     this.totalQty = oldCart.totalQty || 0;
     this.totalPrice = oldCart.totalPrice || 0;
 
+    // 4 functions that define the behavior for the app
+    // add() takes in the item and its id to be able to add it to the cart of user
     this.add = function(item, id) {
         var storedItem = this.items[id];
         if (!storedItem) {
@@ -14,6 +16,7 @@ module.exports = function Cart(oldCart) {
         this.totalPrice += storedItem.item.price;
     };
 
+    // reduceByOne() takes in the id and reduces the quantity of the sticker by 1 from home page
     this.reduceByOne = function(id) {
         this.items[id].qty--;
         this.items[id].price -= this.items[id].item.price;
@@ -25,12 +28,14 @@ module.exports = function Cart(oldCart) {
         }
     };
 
+    // removeItem() takes in an id and removes all quantities for a given sticker
     this.removeItem = function(id) {
         this.totalQty -= this.items[id].qty;
         this.totalPrice -= this.items[id].price;
         delete this.items[id];
     };
     
+    // generateArray() creates an array of the stickers in the cart
     this.generateArray = function() {
         var arr = [];
         for (var id in this.items) {
